2c7736bc2bc460e37cbe2aab41a370b7a7456090
João Jungblut <99040917+JoaoJungblut@users.noreply.github.com>
2023-11-23 15:46:23 -0300

    .

--- venv/Lib/site-packages/farama_notifications/__init__.py
+++
@@ -1,8 +0,0 @@
-# notifications are a dict of dicts
-notifications = dict()
-
-###############################################################
-# Example notification for project/version, follow this format
-###############################################################
-# notifications["project_name"] = dict()
-# notifications["project_name"]["version_number"] = "Notification message."
--- venv/Lib/site-packages/gymnasium/envs/tabular/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Provides Tabular JAX FuncEnv implementations."""
-
-from gymnasium.envs.tabular.blackjack import BlackJackJaxEnv
-from gymnasium.envs.tabular.cliffwalking import CliffWalkingJaxEnv
